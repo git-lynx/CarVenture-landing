4f1e0d29e71fe0ce4c915d2647df415315db6dc8
Dmitriy Spitsyn <generation21x@gmail.com>
2022-02-03 03:22:16 +0200

    initial commit

---
+++ app/js/burger-menu.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function() {
+    $('.burger-menu').click(function(event){
+        $('.burger-menu').toggleClass('active');
+        $('.burger-nav').toggleClass('active');
+        $('body').toggleClass('fixed-page');
+    });
+});
